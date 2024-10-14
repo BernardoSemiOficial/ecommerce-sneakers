2357c2d42d511e92bd2e2a1c8652d325871ae9a7
Bernardo Pereira <bernardopereiraoliveira258@gmail.com>
2024-10-13 23:16:54 -0300

    chore: ajustes no frontend e no backend

--- backend/src/controller/Payment.ts
+++ backend/src/controller/Payment.ts
@@ -1,59 +1,55 @@
-import { mercadopago } from "../gateway/mercadoPago";
-import { Connection } from "../database/connection";
 import { Request, Response } from "express";
+import { Connection } from "../database/connection";
+import { mercadopago } from "../gateway/mercadoPago";
 
 const Payment = {
+	async getPayment(req: Request, res: Response) {
+		try {
+		} catch (error) {
+			return res.status(400).json({ error_message: error.message, error });
+		}
+	},
 
-    async getPayment(req: Request, res: Response) {
+	async postPayment(req: Request, res: Response) {
+		console.log(req.body);
 
-        try {
-            
-        } catch (error) {
-            return res.status(400).json({ error_message: error.message, error })
-        }
+		const order = req.body;
 
-    },
-    
-    async postPayment(req: Request, res: Response) {
-        console.log(req.body);
+		const values = [
+			order.num_order,
+			order.date_order,
+			order.qtda_products,
+			order.total_order,
+			order.id_customer,
+		];
 
-        const order = req.body;
+		try {
+			const responseGateway = await mercadopago.payment.create(req.body);
+			const responseBody = await responseGateway.body;
+			const { status, status_detail, id, payment_type_id } = responseBody;
 
-        const values = [
-            order.num_order,
-            order.date_order,
-            order.qtda_products,
-            order.total_order,
-            order.id_customer
-        ];
+			console.log(responseBody);
+			values.push(id, status, payment_type_id);
 
-        try {
-            // const responseGateway = await mercadopago.payment.save(req.body);
-            const responseGateway = await mercadopago.payment.create(req.body);
-            const { status, status_detail, id } = await responseGateway.body;
-            
-            const configDatabase = {
-                connectionString: process.env.DATABASE_URL || "local"
-            }
-            const connectionDatabase = new Connection(configDatabase);
-    
-            const sql = `INSERT INTO
-                            TB_ORDER (num_order, date_order, qtda_products, total_order, id_customer)
-                            VALUES (?, ?, ?, ?, ?)`; 
+			const configDatabase = {
+				connectionString: process.env.DATABASE_URL || "local",
+			};
+			const connectionDatabase = new Connection(configDatabase);
 
-            const statusInsert = await connectionDatabase.query(sql, values);
-            console.log(statusInsert);
+			const sql = `INSERT INTO
+										TB_ORDER (num_order, date_order, qtda_products, total_order, id_customer, id_transaction, status_payment, method_payment)
+										VALUES ($1, $2, $3, $4, $5, $6, $7, $8)`;
 
-            res.status(responseGateway.status).json({ status, status_detail, id });
+			const statusInsert = await connectionDatabase.query(sql, values);
+			console.log(statusInsert);
 
-        } catch (error) {
-            console.log(error);
-            console.log(error.message);
-            return res.status(400).json({ error });
-        }
-
-    },
-
-}
+			res.status(responseGateway.status).json({ status, status_detail, id });
+		} catch (error) {
+			console.log(error);
+			console.log(error.message);
+			return res.status(400).json({ error });
+		}
+	},
+};
 
 export { Payment };
--- frontend/src/pages/Collection/index.jsx
+++ frontend/src/pages/Collection/index.jsx
@@ -6,44 +6,44 @@ import { instanceAxios } from "../../services/axios";
 import "./styles.scss";
 
 function Collection() {
-
 	const navigate = useNavigate();
 
 	const [products, setProducts] = useState([]);
 
 	useEffect(() => {
-
 		const requestGetProducts = async () => {
-			const response = await instanceAxios.get('http://localhost:4000/products');
+			const response = await instanceAxios.get(
+				"http://localhost:4000/products"
+			);
 			const newStateProducts = response.data.products;
 			setProducts(newStateProducts);
-		}
+		};
 
 		requestGetProducts();
-
-	}, [])
+	}, []);
 
 	function formatMoney(number) {
-		return new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' })
-			.format(number);
+		return new Intl.NumberFormat("pt-BR", {
+			style: "currency",
+			currency: "BRL",
+		}).format(number);
 	}
 
 	return (
 		<main className="sn__collection">
-
 			<div className="sn__collection-wrapper">
-				
 				<h1 className="sn__collection-title">Sneakers</h1>
 
 				<ul className="sn__collection-products">
-
-					{products.map(product => (
+					{products.map((product) => (
 						<li className="sn__collection-product" key={product.id_product}>
 							<figure className="sn__collection-product-image">
 								<img
 									src={product.url_images[0]}
 									alt={product.title}
 									title={product.title}
+									width={360}
+									height={360}
 								/>
 							</figure>
 							<div className="sn__collection-product-info">
@@ -62,11 +62,8 @@ function Collection() {
 							</div>
 						</li>
 					))}
-
 				</ul>
-
 			</div>
-
 		</main>
 	);
 }
